0845e374749c22a16dfd7942f2b244c625333e16
Omkar Deshmukh <132034602+omkardwork603@users.noreply.github.com>
2026-06-17 11:35:41 +0530

    Add files via upload

---
+++ prime-number.php
@@ -0,0 +1,22 @@
+<!-- prime number -->
+ <!-- leap year -->
+
+
+ <?php
+ $num = 11;
+ $flag = 0;
+
+for($i = 2; $i < $num; $i++){
+    if($num % $i == 0){
+        $flag = 1;
+        break;
+    }
+}
+
+
+if($flag == 0){
+    echo "This is a prime number";
+}else{
+    echo "This is a not prime number";
+}
+ ?>
