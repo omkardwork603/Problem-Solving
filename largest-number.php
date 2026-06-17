f0c9a76d48d64d078978154f6becaf70175d7013
Omkar Deshmukh <132034602+omkardwork603@users.noreply.github.com>
2026-06-17 12:08:28 +0530

    Add files via upload

--- largest-number.php
+++ largest-number.php
@@ -1,18 +1,74 @@
 <!-- largest number -->
+<!-- second largest number -->
+ <!-- third largest number -->
+
 
 
  <?php
  
-$arr = [10, 323, 878, 343, 23232];
-$largest = $arr[0];
+$arr = [1000, 3000, 4000, 5000, 600];
 
-for($i = 0; $i < count($arr); $i++){
-if($arr[$i] < $largest){
-   $largest = $arr[$i];
-}
+for($i = 0; isset($arr[$i]); $i++){
+   for($j = $i + 1; isset($arr[$j]); $j++){
+      if($arr[$i] > $arr[$j]){
+         $temp = $arr[$i];
+         $arr[$i] = $arr[$j];
+         $arr[$j] = $temp;
+      }
+   }
 }
 
-echo $largest;
+
+
+   echo "largest number: ". $arr[4] . "<br>";
+
+
+ ?>
+
+ <!-- 2 second largest number -->
+
+ 
+ <?php
+ 
+$arr = [1000, 3000, 4000, 5000, 600];
+
+for($i = 0; isset($arr[$i]); $i++){
+   for($j = $i + 1; isset($arr[$j]); $j++){
+      if($arr[$i] > $arr[$j]){
+         $temp = $arr[$i];
+         $arr[$i] = $arr[$j];
+         $arr[$j] = $temp;
+      }
+   }
+}
+
+
+
+   echo " Second largest number: ". $arr[3] . "<br>";
+
+
+ ?>
+
+ <!-- 3 third largest number -->
+
+ 
+ <?php
+ 
+$arr = [1000, 3000, 4000, 5000, 600];
+
+for($i = 0; isset($arr[$i]); $i++){
+   for($j = $i + 1; isset($arr[$j]); $j++){
+      if($arr[$i] > $arr[$j]){
+         $temp = $arr[$i];
+         $arr[$i] = $arr[$j];
+         $arr[$j] = $temp;
+      }
+   }
+}
+
+
+
+   echo " Third largest number: ". $arr[2];
 
 
  ?>
