08007bb0d4020acd18eb38e287dbde6400adc091
Omkar Deshmukh <132034602+omkardwork603@users.noreply.github.com>
2026-06-18 17:42:25 +0530

    Add files via upload

--- Find-Missing-Number.php
+++ Find-Missing-Number.php
@@ -1,17 +1,24 @@
 <?php
+
+// Find Missing Number
+
 $arr = [1,2,3,5];
+
 $expectedSum = 0;
 $actualSum = 0;
 
 $n = 5; // Last number should be 5
+
 for($i=1; $i<=$n; $i++)
 {
     $expectedSum += $i;
 }
 
-for($i=0; isset($arr[$i]); $i++){
+for($i=0; isset($arr[$i]); $i++)
+{
     $actualSum += $arr[$i];
 }
 
 echo "Missing Number = ".($expectedSum - $actualSum);
+
 ?>
