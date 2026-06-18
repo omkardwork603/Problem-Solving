4d6984a2103352f5ae01350c9afa7e617d8eadbe
Omkar Deshmukh <132034602+omkardwork603@users.noreply.github.com>
2026-06-18 17:31:02 +0530

    Create Find-Missing-Number.php

---
+++ Find-Missing-Number.php
@@ -0,0 +1,17 @@
+<?php
+$arr = [1,2,3,5];
+$expectedSum = 0;
+$actualSum = 0;
+
+$n = 5; // Last number should be 5
+for($i=1; $i<=$n; $i++)
+{
+    $expectedSum += $i;
+}
+
+for($i=0; isset($arr[$i]); $i++){
+    $actualSum += $arr[$i];
+}
+
+echo "Missing Number = ".($expectedSum - $actualSum);
+?>
